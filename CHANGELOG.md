c9b9ceec4c84cb244ab1f76ef58a40f1c5de717f
Wilmer Arambula <terabytesoftw@gmail.com>
2026-06-21 11:41:35 -0400

    chore: integrate scaffold-managed development metadata and configuration.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on Keep a Changelog, and this project adheres to Semantic Ve
 
 ## 0.1.1 Under development
 
+### Changed
+
+- chore: integrate scaffold-managed development metadata and configuration.
+
 ## 0.1.0 April 16, 2026
 
 - feat: initial `yii2-extensions/inertia` package structure.
--- ecs.php
+++ ecs.php
@@ -2,12 +2,22 @@
 
 declare(strict_types=1);
 
-/** @var \Symplify\EasyCodingStandard\Configuration\ECSConfigBuilder $ecsConfigBuilder */
-$ecsConfigBuilder = require __DIR__ . '/vendor/php-forge/coding-standard/config/ecs.php';
+use PhpCsFixer\Fixer\Whitespace\HeredocIndentationFixer;
+use Symplify\CodingStandard\Fixer\Commenting\RemoveDeadVarThisFixer;
 
-return $ecsConfigBuilder->withPaths(
-    [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ],
-);
+/** @var \Symplify\EasyCodingStandard\Configuration\ECSConfigBuilder $builder */
+$builder = require __DIR__ . '/vendor/php-forge/coding-standard/src/ecs-83.php';
+
+return $builder
+    ->withPaths(
+        [
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    )
+    ->withSkip(
+        [
+            HeredocIndentationFixer::class,
+            RemoveDeadVarThisFixer::class => [__DIR__ . '/src/views'],
+        ],
+    );
